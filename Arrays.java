a67132d4a03da936f5cd10c25a3bc1857e70a240
Ujwal Parashar <88845963+UjwalParashar9890@users.noreply.github.com>
2024-02-29 00:01:14 +0530

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arrays {
+    public static void main (String args[]){
+        int[] marks = new int[3];
+        marks[0]=76; //physics
+        marks[1]=45; //chemistry
+        marks[2]=89; //maths
+
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
